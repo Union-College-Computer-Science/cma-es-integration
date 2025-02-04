cbc819e710f2978e0345e4fe76a3adb37d726228
tbreimer <breimert@union.edu>
2025-02-04 15:34:01 -0500

    output.csv updates now

--- run_cmaes.py
+++ run_cmaes.py
@@ -6,13 +6,11 @@ Author: Thomas Breimer
 February 4th, 2025
 """
 
-import os
 import sys
-import pathlib
+import csv
 import run_simulation as sim
-import CMA
+from cmaes import CMA
 import numpy as np
-import pandas as pd
 
 def run_cma_es(gens, sigma_val):
     """
@@ -26,14 +24,19 @@ def run_cma_es(gens, sigma_val):
         used to generate new candidate solutions
     """
 
-    # Generate Results DF
-    df_cols = ['Generation', 'Individual', 'Fitness']
+    # Generate output.csv file
+
+    csv_header = ['Generation', 'Best-Fitness']
 
     for i in range(sim.NUM_ACTUATORS):
-        df_cols = df_cols + ['frequency' + str(i), 'amplitude' + str(i),
+        csv_header = csv_header + ['frequency' + str(i), 'amplitude' + str(i),
                              'phase_offset' + str(i)]
 
-    df = pd.DataFrame(columns=df_cols)
+    with open("output.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(csv_header) 
+
+    # Init CMA
 
     optimizer = CMA(mean=np.array([sim.AVG_FREQ, sim.AVG_AMP, sim.AVG_PHASE_OFFSET] * sim.NUM_ACTUATORS),
                     sigma=sigma_val)
@@ -43,31 +46,31 @@ def run_cma_es(gens, sigma_val):
 
         for indv_num in range(optimizer.population_size):
             x = optimizer.ask()
-            value = sim.run_simulation(sim.NUM_ITERS, x, False)
-            solutions.append((x, value))
-            to_add = [generation, indv_num, value] + list(x)
-            df.loc[len(df)] = to_add
-
+            fitness = sim.run(sim.NUM_ITERS, x, False)
+            solutions.append((x, fitness))
 
         optimizer.tell(solutions)
         print([i[1] for i in solutions])
         print("Generation", generation, "Best Fitness:", solutions[0][1])
 
-    # Save csv
-    this_dir = pathlib.Path(__file__).parent.resolve()
-    df.to_csv(os.path.join(this_dir, 'output.csv'), index=False)
+        # Add a new row to output.csv file with cols: generation#, fitness, and genome
+        new_row = [generation, solutions[0][1]] + solutions[0][0].tolist()
+
+        with open("output.csv", "a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(new_row)
 
 
 if __name__ == "__main__":
     args = sys.argv
 
     if len(args) > 1:
-        NUM_GENS = int(args[1])
+        sim.NUM_GENS = int(args[1])
 
     if len(args) > 2:
-        SIGMA = float(args[2])
+        sim.SIGMA = float(args[2])
 
-    run_cma_es(NUM_GENS, SIGMA)
+    run_cma_es(sim.NUM_GENS, sim.SIGMA)
 
 
 
